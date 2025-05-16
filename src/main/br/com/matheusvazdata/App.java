6430d8c6e1d390163549b220aadd7ac5ad1ec336
Matheus <matheusvaz.data@gmail.com>
2025-05-15 22:17:09 -0300

    refactor: remove deprecated App.java to adopt modular structure

--- src/main/br/com/matheusvazdata/App.java
+++
@@ -1,7 +0,0 @@
-package br.com.matheusvazdata;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Conversor de Moeda funcionando com Maven!");
-    }
-}
